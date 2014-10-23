24f68bedb6818fdafcc92743058ba365315b62d3
canozdtr <canozd98@gmail.com>
2014-10-23 21:42:56 +0300

    v0.000001

---
+++ HazirlikApp/HomeViewController.swift
@@ -0,0 +1,27 @@
+//
+//  HomeViewController.swift
+//  HazirlikApp
+//
+//  Created by Can Özdemir on 23/10/14.
+//  Copyright (c) 2014 Can Özdemir. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    @IBOutlet weak var homeImage: UIImageView!
+    @IBOutlet weak var homeText: UITextView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
