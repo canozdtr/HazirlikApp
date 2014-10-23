7e4faecda073b524af671ffd26f1b8307f8d3cc4
Can Özdemir <canozdemir@Can-Ozdemirs-MacBook-Pro.local>
2014-10-23 21:15:24 +0300

    Initial Commit

---
+++ HazirlikApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HazirlikApp
+//
+//  Created by Can Özdemir on 23/10/14.
+//  Copyright (c) 2014 Can Özdemir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
